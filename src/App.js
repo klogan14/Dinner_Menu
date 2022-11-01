9fcc59a075e02d1f7c75c7721e89666b23727221
Kyle Logan <logan.kyle@mayo.edu>
2022-11-01 08:13:29 -0500

    Remove unused Link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header';
 import Menu from './components/Menu';
 
