c107cab5ad166d8eccb328a8d897045b64a6c2da
Kyle Logan <logan.kyle@mayo.edu>
2022-11-01 08:11:15 -0500

    Remove eslint warnings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
-import { useState } from 'react';
 import Header from './components/Header';
 import Menu from './components/Menu';
 
 function App() {
 
-  const [open, setOpen] = useState(true);
 
   return (
     <Router>
